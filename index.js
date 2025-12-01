dc081e0d69ddcaa6d0f40c4db5e18ab0ec932fa4
Ajay94-coder <ajaynailwal74@gmail.com>
2025-12-01 21:13:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+const app = express();
+const port = 4000;
+
+app.get('/',(req,res)=>{
+    res.send('hello world');
+
+})
+app.listen(process.env.PORT,()=>{
+    console.log(`server is running on port ${port}`)
+})
